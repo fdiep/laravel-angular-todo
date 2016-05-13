def73e9af2d3c04f85068fbfae7ad64b38a1e788
Felipe Diep <fdiep@blim.com>
2016-05-13 12:10:36 -0400

    DB seed users and todos

--- project/app/app/Todo.php
+++ project/app/app/Todo.php
@@ -8,6 +8,9 @@ class Todo extends Model
 {
     protected $fillable = ['description','owner_id','is_done'];
 
+    /**
+     * Get the user that owns this todo.
+     */
     public function owner()
     {
         return $this->belongsTo('App\User','owner_id');
