d2c8ce03daea11c008a6892072302e77bb3c1e24
Felipe Diep <fdiep@blim.com>
2016-05-17 16:53:33 -0400

    Update API tests

--- project/app/tests/api/AuthTest.php
+++ project/app/tests/api/AuthTest.php
@@ -1,43 +1,93 @@
 <?php
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Http\Controllers\ErrorCode AS ErrorCode;
 
 class AuthTest extends TestCase
 {
-  use DatabaseTransactions;
-
-  /**
-   * Auth Urls
-   */
-   private $registerUrl;
-   private $authUrl;
-   private $userUrl;
+    use DatabaseTransactions;
 
    /**
-    * Build all urls
+    * Auth Urls.
     */
-   function __construct() {
-      $this->registerUrl = $this->apiUrl . '/register';
-      $this->authUrl = $this->apiUrl . '/authenticate';
-      $this->userUrl = $this->authUrl . '/user';
-   }
+    private $authUrl;
+    private $userUrl;
+
+   /**
+    * Build all urls.
+    */
+     public function __construct()
+     {
+         $this->authUrl = $this->apiUrl.'/authenticate';
+         $this->userUrl = $this->authUrl.'/user';
+     }
 
     /**
-     * Test registration with valid user payload.
+     * Test authenticate with valid user payload.
      */
-    public function testRegistration()
-    {
-        $user = factory(App\User::class)->make();
-        $payload = [
-          'name' => $user->name,
-          'email' => $user->email,
-          'password' => $user->password
-        ];
+      public function testValidAuthentication()
+      {
+          $passwordRaw = str_random(10);
+          $user = factory(App\User::class)->create([
+            'password' => bcrypt($passwordRaw),
+          ]);
+          $payload = [
+            'email' => $user->email,
+            'password' => $passwordRaw,
+          ];
 
-        $this->json('POST', $this->registerUrl, $payload)
+          $this->json('POST', $this->authUrl, $payload)
+            ->seeJsonStructure([
+              'data' => [
+                'data' => [
+                  'token',
+                ],
+              ],
+            ])
+            ->seeJson([
+              'success' => true,
+            ]);
+      }
+
+     /**
+      * Test authenticate with invalid user email.
+      */
+       public function testInvalidEmailAuthentication()
+       {
+           $passwordRaw = str_random(10);
+           $user = factory(App\User::class)->create([
+             'password' => bcrypt($passwordRaw),
+           ]);
+           $payload = [
+             'email' => $user->email.'bademail',
+             'password' => $passwordRaw,
+           ];
+
+           $this->json('POST', $this->authUrl, $payload)
              ->seeJson([
-                 'name' => $user->name,
-                 'email' => $user->email,
+               'success' => false,
+               'code' => ErrorCode::NOT_AUTHORIZED
              ]);
-    }
+       }
+
+      /**
+       * Test authenticate with invalid user password.
+       */
+        public function testInvalidPasswordAuthentication()
+        {
+            $passwordRaw = str_random(10);
+            $user = factory(App\User::class)->create([
+              'password' => bcrypt($passwordRaw),
+            ]);
+            $payload = [
+              'email' => $user->email,
+              'password' => $passwordRaw.'badpassword',
+            ];
+
+            $this->json('POST', $this->authUrl, $payload)
+              ->seeJson([
+                'success' => false,
+                'code' => ErrorCode::NOT_AUTHORIZED
+              ]);
+        }
 }
