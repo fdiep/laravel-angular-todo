e822a596198277b3b06ebdb582cd14dc858ba420
Felipe Diep <fdiep@blim.com>
2016-05-20 13:31:50 -0400

    cast Todo variables

--- project/app/app/Todo.php
+++ project/app/app/Todo.php
@@ -8,6 +8,13 @@ class Todo extends Model
 {
     protected $fillable = ['description','owner_id','is_done'];
 
+    // make sure following fields are returned in correct type
+    protected $casts = [
+      'id' => 'integer',
+      'owner_id' => 'integer',
+      'is_done' => 'boolean',
+    ];
+
     /**
      * Get the user that owns this todo.
      */
